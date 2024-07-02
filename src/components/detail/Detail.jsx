f8de3b39de7fe385fbcbf7b7c6db023c93e718b9
Alok Jha <axjh03@gmail.com>
2024-07-02 15:12:05 -0500

    details

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,49 +1,119 @@
 import "./detail.css";
 const Detail = () => {
-    return (
-        <div className="detail">
-            <div className="user">
-                <img src="./avatar.png" alt="" />
-                <h2>John Doe</h2>
-                <p>Lorem ipsum dolor sit.</p>
-            </div>
+  return (
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>John Doe</h2>
+        <p>Lorem ipsum dolor sit.</p>
+      </div>
 
-           <div className="info">
-
-            <div className="option">
-                <div className="title">
-                    <span>Chat Settings</span>
-                    <img src="./arrowUp.png" alt="" />
-                </div>
-            </div>
-            
-            <div className="option">
-                <div className="title">
-                    <span>Privacy & Help</span>
-                    <img src="./arrowUp.png" alt="" />
-                </div>
-            </div>
-
-            <div className="option">
-                <div className="title">
-                    <span>Shared Photos</span>
-                    <img src="./arrowDown.png" alt="" />
-                </div>
-
-                <div className="photos">
-                    
-                </div>
-            </div>
-
-            <div className="option">
-                <div className="title">
-                    <span>Chat Settings</span>
-                    <img src="./arrowUp.png" alt="" />
-                </div>
-            </div>
-            </div> 
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
         </div>
-    )
-}
+
+        <div className="option">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://prexamples.com/wp-content/uploads/2013/04/hp-ball-wi-fi-pictures.jpg"
+                  alt=""
+                />
+
+                <span>photo_2024_2.jpg</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://prexamples.com/wp-content/uploads/2013/04/hp-ball-wi-fi-pictures.jpg"
+                  alt=""
+                />
+
+                <span>photo_2024_2.jpg</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://prexamples.com/wp-content/uploads/2013/04/hp-ball-wi-fi-pictures.jpg"
+                  alt=""
+                />
+
+                <span>photo_2024_2.jpg</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://prexamples.com/wp-content/uploads/2013/04/hp-ball-wi-fi-pictures.jpg"
+                  alt=""
+                />
+
+                <span>photo_2024_2.jpg</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://prexamples.com/wp-content/uploads/2013/04/hp-ball-wi-fi-pictures.jpg"
+                  alt=""
+                />
+
+                <span>photo_2024_2.jpg</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://prexamples.com/wp-content/uploads/2013/04/hp-ball-wi-fi-pictures.jpg"
+                  alt=""
+                />
+
+                <span>photo_2024_2.jpg</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+          </div>
+        </div>
+
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
+  );
+};
 
 export default Detail;
